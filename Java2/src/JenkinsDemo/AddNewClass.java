39628a3368dd64c9c22d0207060e4e77f28af455
venkatgiri <venkatgiri@192.168.43.117>
2020-09-20 12:31:46 +0530

    added a new class to understand what is happening

---
+++ Java2/src/JenkinsDemo/AddNewClass.java
@@ -0,0 +1,10 @@
+package JenkinsDemo;
+
+public class AddNewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Added this to infer what is happening");
+	}
+
+}
