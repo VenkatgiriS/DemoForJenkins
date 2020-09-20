1f85a93e0584928ef163f3f9dbef8833db4e6f89
venkatgiri <venkatgiri@192.168.43.117>
2020-09-20 12:26:07 +0530

    commited second time

--- Java2/src/JenkinsDemo/HelloWorld.java
+++ Java2/src/JenkinsDemo/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello!!! Learn Jenkins");
+			System.out.println("Learn Kuburnetes as well");
 	}
 
 }
