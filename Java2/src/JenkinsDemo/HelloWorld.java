03a2b2f525c91d75cce3b707440bc92c1a656a46
venkatgiri <venkatgiri@192.168.43.117>
2020-09-20 12:23:51 +0530

    initial commit 1

---
+++ Java2/src/JenkinsDemo/HelloWorld.java
@@ -0,0 +1,10 @@
+package JenkinsDemo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello!!! Learn Jenkins");
+	}
+
+}
